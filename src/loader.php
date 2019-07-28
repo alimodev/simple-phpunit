c3f2cf4b31a9cedcb78d35d29ee01b5871c73da6
Alireza Mortazavi <trans_paypal@yahoo.com>
2019-07-28 22:28:55 +0430

    Clarifying loader procedures

--- src/loader.php
+++ src/loader.php
@@ -4,8 +4,10 @@
  * Loading UnitTest classes
  */
 spl_autoload_register(function($className){
+	// removing the namespace
 	$className = explode('\\', $className);
   $className = end($className);
+	// adding dir and ext
 	$classFile = __DIR__ . DIRECTORY_SEPARATOR . $className . '.class.php';
 	if (file_exists($classFile))
 	{
